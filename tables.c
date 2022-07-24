35b3c4ecd7f2ed0738671d4cb129f7e0b146f259
Vishruth Kumar <70757196+Vishruthh@users.noreply.github.com>
2022-07-24 19:46:59 +0530

    Create tables.c

---
+++ tables.c
@@ -0,0 +1,22 @@
+//Math Tables generator using C by Vishruth Codes
+#include<stdio.h>
+
+void tables(int n, int s, int e)
+{
+    for(int i=s;i<=e;i++)
+    {
+        printf("\n%d x %d = %d",n,i,n*i);
+    }
+}
+
+int main()
+{
+    int n,srange,erange;
+    printf("\nEnter the Number: ");
+    scanf("%d",&n);
+    printf("\nEnter the starting range: ");
+    scanf("%d",&srange);
+    printf("\nEnter the ending range: ");
+    scanf("%d",&erange);
+    tables(n,srange,erange);
+}
