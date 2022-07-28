abbc68679b89281c9b8ddd6a9b8b3eaa3418fc26
Vishruth Kumar <70757196+Vishruthh@users.noreply.github.com>
2022-07-28 09:49:19 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,33 +1,43 @@
+//Program to generate fibonacci series using recursion by Vishruth Codes
 #include<iostream>
 using namespace std;
 
-template <typename t>
-t mem<t>[10]=-1;
 
-template <typename t>
-t fib(t n)
+int fib(int n, int ar[])
 {
     //t mem[n]=-1;
     if(n<=1)
     {
-        mem<t>[n]=n;
+        ar[n]=n;
         return n;
     }
     else
     {
-        if(mem<t>[n-2]==-1)
-            mem<t>[n-2]=fib(n-2);
-        if(mem<t>[n-1]==-1)
-            mem<t>[n-1]=fib(n-1);
-        return (mem<t>[n-2]+mem<t>[n-1]);
+        if(ar[n-2]==-1)
+            ar[n-2]=fib(n-2,ar);
+        if(ar[n-1]==-1)
+            ar[n-1]=fib(n-1,ar);
+
+        ar[n]=ar[n-2]+ar[n-1];
+        return (ar[n-2]+ar[n-1]);
     }
 }
 
 int main()
 {
     int n;
-    cout<<"\nEnter the number: ";
+    cout<<"\nEnter the count: ";
     cin>>n;
-    fib<int>(n);
+    int ar[n];
+    for(int i=0;i<=n;i++)
+    {
+        ar[i]=-1;
+    }
+    printf("The term %d (starting from 0) in fibonacci series is: %d",n,fib(n,ar));
+    cout<<"\nThe series: ";
+    for(int i=0;i<=n;i++)
+    {
+        cout<<ar[i]<<", ";
+    }
     return 0;
 }
