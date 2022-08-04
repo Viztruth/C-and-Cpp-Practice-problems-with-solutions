97867a1b9e3feb3dcc147d8cf97e78f14903d8c0
Vishruth Kumar <70757196+Vishruthh@users.noreply.github.com>
2022-08-04 06:27:16 +0530

    Add files via upload

---
+++ stringasparam.c
@@ -0,0 +1,20 @@
+//C program to pass string as parameter to a function | Vishruth Codes
+#include<stdio.h>
+
+void displaystr(char str[], int n)
+{
+    printf("\nThe string is: ");
+    puts(str);
+}
+
+int main()
+{
+    int n;
+    printf("\nEnter the size of the string: ");
+    scanf("%d",&n);
+    char str[n];
+    getchar();
+    printf("\nEnter the string: ");
+    gets(str);
+    displaystr(str,n);
+}
